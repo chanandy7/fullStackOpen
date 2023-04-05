bc5edbfb3d8babae80726170fcd4e1d79739d5db
Andy Chan <andy.chan7@hotmail.co.uk>
2023-04-06 00:02:34 +1200

    Update App.js

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -4,9 +4,22 @@ const Header = (props) => {
     )
 }
 
-const Content = (prop) => {
+const Part = (prop) => {
     return (
         <p>{prop.part} {prop.exercise}</p>
+    )
+}
+
+const Content = (prop) => {
+    return (
+        <div>
+            <Part part={prop.parts[0]} exercise={[prop.exercise[0]]} />
+            {/* <Part/> */}
+            {/* <Part/> */}
+        </div>
+
+
+        // <p>{prop.part} {prop.exercise}</p>
     )
 }
 const Total = (p) => {
@@ -27,9 +40,8 @@ const App = () => {
     return (
         <div>
             <Header name={course} />
-            <Content part={part1} exercise={exercises1} />
-            <Content part={part2} exercise={exercises2} />
-            <Content part={part3} exercise={exercises3} />
+            <Content parts={[part1, part2, part3]} exercise={[exercises1, exercises2, exercises3]} />
+
             <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
 
         </div>
